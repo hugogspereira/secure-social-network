8ace6214abf0865d29650abc3bc9e6161634e84b
Hugo Pereira <hugogspereira@gmail.com>
2023-04-15 16:06:22 +0100

    logout (jsp and servlet)

---
+++ WEB-INF/logout.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logout</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        label {
+            display: inline-block;
+            width: 150px;
+            text-align: right;
+            margin-right: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Logout</h1>
+    <p>You have been successfully logged out.</p>
+    <p><a href="authenticateUser.jsp">Back to Login Page</a></p>
+</body>
+</html>
--- src/servlet/LogoutServlet.java
+++ src/servlet/LogoutServlet.java
@@ -7,11 +7,13 @@ import exc.AccountDoesNotExist;
 import exc.AccountIsLocked;
 import exc.AuthenticationError;
 import exc.EncryptionDontWork;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet(name="Logout", urlPatterns={"/Logout"})
 public class LogoutServlet extends HttpServlet {
@@ -25,27 +27,39 @@ public class LogoutServlet extends HttpServlet {
         // (Auth) getServletContext().getAttribute("authenticator");
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/logout.jsp").forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             Acc authUser = auth.checkAuthenticatedRequest(request, response);
             auth.logout(authUser);
+
             HttpSession session = request.getSession(false);
             if (session != null) {
                 session.invalidate();
             }
-        } catch (AuthenticationError e) {
-            throw new RuntimeException(e);
+
+            // Redirect to login page after successful logout
+            response.sendRedirect(request.getContextPath() + "/AuthenticateUser");
+        }
+        catch (AuthenticationError e) {
+            request.setAttribute("errorMessage", "Invalid username or password");
+            request.getRequestDispatcher("/WEB-INF/authenticateUser.jsp").forward(request, response);
         }
         catch (AccountIsLocked e) {
-            throw new RuntimeException(e);
+            request.setAttribute("errorMessage", "Your account is locked");
+            request.getRequestDispatcher("/WEB-INF/authenticateUser.jsp").forward(request, response);
         }
         catch (EncryptionDontWork e) {
-            throw new RuntimeException(e);
+            request.setAttribute("errorMessage", "Problems with encryption");
+            request.getRequestDispatcher("/WEB-INF/authenticateUser.jsp").forward(request, response);
         }
         catch (AccountDoesNotExist e) {
-            throw new RuntimeException(e);
+            request.setAttribute("errorMessage", "The Root account does not exist");
+            request.getRequestDispatcher("/WEB-INF/authenticateUser.jsp").forward(request, response);
         }
-
     }
 
 }
