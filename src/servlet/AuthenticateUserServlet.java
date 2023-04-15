df5c9ada0bbc8096e3fdd55f70d65d48a4dabf38
Hugo Pereira <hugogspereira@gmail.com>
2023-04-15 13:18:58 +0100

    worked on AuthenticateUser

--- src/servlet/AuthenticateUserServlet.java
+++ src/servlet/AuthenticateUserServlet.java
@@ -7,11 +7,13 @@ import exc.AccountDoesNotExist;
 import exc.AccountIsLocked;
 import exc.AuthenticationError;
 import exc.EncryptionDontWork;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet(name="AuthenticateUser", urlPatterns={"/AuthenticateUser"})
 public class AuthenticateUserServlet extends HttpServlet {
@@ -25,26 +27,36 @@ public class AuthenticateUserServlet extends HttpServlet {
         // (Auth) getServletContext().getAttribute("authenticator");
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/AuthenticateUser.jsp").forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
         try {
-            String name = request.getParameter("username");
-            String pwd = request.getParameter("password");
-            Acc authuser = auth.authenticateUser(name, pwd);
+            Acc authUser = auth.authenticateUser(username, password);
 
             HttpSession session = request.getSession(true);
-            session.setAttribute("JWT", authuser.getJWT());
-        }
-        catch (AccountIsLocked e) {
-            e.printStackTrace();
-        }
-        catch (EncryptionDontWork e) {
-            e.printStackTrace();
+            session.setAttribute("JWT", authUser.getJWT());
+            response.sendRedirect(request.getContextPath() + "/manageusers");
         }
         catch (AuthenticationError e) {
-            e.printStackTrace();
+            request.setAttribute("errorMessage", "Invalid username or password");
+            request.getRequestDispatcher("/WEB-INF/AuthenticateUser.jsp").forward(request, response);
+        }
+        catch (AccountIsLocked e) {
+            request.setAttribute("errorMessage", "Account is locked");
+            request.getRequestDispatcher("/WEB-INF/AuthenticateUser.jsp").forward(request, response);
+        }
+        catch (EncryptionDontWork e) {
+            request.setAttribute("errorMessage", "Problems with encryption");
+            request.getRequestDispatcher("/WEB-INF/AuthenticateUser.jsp").forward(request, response);
         }
         catch (AccountDoesNotExist e) {
-            e.printStackTrace();
+            request.setAttribute("errorMessage", "Account does not exist");
+            request.getRequestDispatcher("/WEB-INF/AuthenticateUser.jsp").forward(request, response);
         }
     }
 
